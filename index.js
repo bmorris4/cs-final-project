bbfd543f320db4e0cc63853cf4d14cbfaa7e4dc0
bmorris <77358614+bmorris4@users.noreply.github.com>
2022-05-25 14:04:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 4506;
+const app = express();
+
+app.get('/api/game/content', (req, res) => {
+  res.json({
+    "title": "title",
+    "background": "https://i.ytimg.com/vi/Ixzd8tYl5K4/maxresdefault.jpg",
+    "message": ""
+  })
+});
+
+app.listen(port, () => {
+  console.log(`server started on port: ${port}`);
+});
